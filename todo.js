c7fa353043488d2abee300b41d5bcc86834f7a9f
Hyeryoung16 <hyeryoung16@gmail.com>
2021-02-04 14:20:07 +0900

    JS: todolist insert local storage

--- todo.js
+++ todo.js
@@ -4,7 +4,16 @@ const todoList = document.querySelector(".js-todoList");
 
 const TODOS_LS = 'todos';
 
-function display(text){
+const todos = [];
+
+function saveTodos(){
+    localStorage.setItem(TODOS_LS, JSON.stringify(todos)); //JSON.stringify : js object를 string으로 바꿔준다 (b/c: )
+}
+
+function addTodo(text){
+    const newId = todos.length + 1;
+
+    /* HTML Level */
     const li = document.createElement("li");
 
     const delBtn = document.createElement("button");
@@ -15,20 +24,32 @@ function display(text){
     span.innerText = text; 
     li.appendChild(span);
 
+    li.id = newId;
     todoList.appendChild(li);
+
+    /* Local Storage Level */
+    const todoObj = {
+        text: text,
+        id: newId
+    };
+    todos.push(todoObj);
+    saveTodos();
 }
 
 function handlerSubmit(event){
     event.preventDefault();
     const currentValue = todoInput.value;
-    display(currentValue);
+    addTodo(currentValue);
     todoInput.value = "";
 }
 
 function loadTodos(){
-    const todos = localStorage.getItem(TODOS_LS);
-    if (todos !== null){
-
+    const loadedTodos = localStorage.getItem(TODOS_LS);
+    if (loadedTodos !== null){
+        const parsedTodo = JSON.parse(loadedTodos); // js obj로 변환
+        parsedTodo.forEach(function(todo){
+            addTodo(todo.text);
+        });
     }
 }
 
